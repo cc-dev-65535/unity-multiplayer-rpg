48ab950e29f63e162a2c060490743f137e2dfb6d
Collin Chan <omiyage4949@gmail.com>
2023-01-06 03:06:50 -0800

    Add template movement

--- Assets/OrbitingCamera.cs
+++ Assets/OrbitingCamera.cs
@@ -6,15 +6,32 @@ public class OrbitingCamera : MonoBehaviour
 {
     public Transform target;
 
+    public float rotSpeed = 1.5f;
+    private float rotY;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotY = transform.eulerAngles.y;
+        offset = target.position - transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once per frame after all Updates for all objects are finished
+    void LateUpdate()
     {
-        
+        float horInput = Input.GetAxis("Horizontal");
+        if (!Mathf.Approximately(horInput, 0))
+        {
+            rotY += horInput * rotSpeed;
+        }
+        else
+        {
+            rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, rotY, 0);
+        transform.position = target.position - (rotation * offset);
+        transform.LookAt(target);
     }
 }
